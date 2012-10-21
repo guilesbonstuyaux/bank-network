f3befc7a3b2d3afa807c545ae2122a2c871ca1a7
guilesbonstuyaux <guilesbonstuyaux@gmail.com>
2012-10-21 23:07:46 +0000

    - add comments

--- banque-api/src/main/java/com/ensimag/api/bank/IBankMessage.java
+++ banque-api/src/main/java/com/ensimag/api/bank/IBankMessage.java
@@ -4,6 +4,12 @@ import java.io.Serializable;
 
 import com.ensimag.api.message.IMessage;
 
+/**
+ * Specific message for banks
+ * 
+ * @author guillaume
+ * 
+ */
 public interface IBankMessage extends IMessage<Serializable, com.ensimag.api.bank.IBankNode> {
 	@Override
 	IBankAction getAction();
